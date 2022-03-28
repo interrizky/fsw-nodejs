1d75263c2499c630ef0aebc68a18b6398b64f325
Rizky Ridho <interrizky@ymail.com>
2022-03-28 18:47:23 +0700

    change connection severity

--- routes/connection.js
+++ routes/connection.js
@@ -3,14 +3,22 @@ const Mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     /* cara 1 */
-    const Conn = await Mongoose.connect(
-      'mongodb://admin:root123@learn-shard-00-00.eedrt.mongodb.net:27017,learn-shard-00-01.eedrt.mongodb.net:27017,learn-shard-00-02.eedrt.mongodb.net:27017/Learn?ssl=true&replicaSet=atlas-goja61-shard-0&authSource=admin&retryWrites=true&w=majority',
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      }
-    )
-    console.log('MongoDB connected : ' +Conn.connection.host)    
+    const options = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // useFindAndModify: false,
+      // useCreateIndex: true,
+      dbName: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      pass: process.env.DB_PASS,
+      ssl: true,
+      replicaSet: "atlas-goja61-shard-0",
+      authSource: process.env.DB_USER,
+      retryWrites: true,
+      w: "majority"
+    };
+    const conn = await mongoose.connect(process.env.DB_URI, options);
+    console.log('MongoDB Connected on Host : ' + conn.connection.host);   
   } catch (error) {
     console.log(error)
     process.exit(1)
